c6377b9950a872a51185afb760b06385f1674a3d
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2019-08-02 19:13:56 -0300

    Criação da Classe Account

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,20 @@
 
 public class Account {
+	
+	private String name; // variável da instância
+	
+	// método para definir o nome no objeto
+	
+	public void setName(String name) {
+		this.name = name; // armazena o nome
+	}
+	
+	// método para recuperar o nome do objeto
+	
+	public String getName() {
+		return name; // retorna o valor do nome para o chamador
+	}
+	
+	// fim da classe Account
 
 }
