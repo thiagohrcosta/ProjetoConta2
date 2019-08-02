31fb81d1d0500044aadb1b10aeb9b49a4b315691
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2019-08-02 20:16:13 -0300

    Modificando o código e adicionando outro tipo de opção

--- src/Account.java
+++ src/Account.java
@@ -2,6 +2,32 @@
 public class Account {
 	
 	private String name; // variável da instância
+	private double balance; // variável da instância
+	
+	// Construtor do Account que recebe dois parâmetros
+	
+	public Account(String name, double balance) {
+		this.name = name; // atribui name à variável de instância name
+		
+		// valida que o balance é maior que 0.0; se não for,
+		// a variável de instância balance mantém seu valor inicial padrão de 0.0
+		
+		if(balance > 0.0) // se o saldo for válido
+			this.balance = balance; // atribui à variável de instância balance
+	}
+	
+	// método que deposita (adiciona) apenas uma quantia válida no saldo
+	
+	public void deposit(double depositAmount) {
+		if (depositAmount > 0.0) // se o depositAmount form válido
+			balance = balance + depositAmount; // adiciona ao saldo
+	}
+	
+	// método retorna o saldo da conta
+	
+	public double getBalance() {
+		return balance;
+	}
 	
 	// método para definir o nome no objeto
 	
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -6,14 +6,31 @@ public class AccountTest {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
+	
+		// cria um objeto Account e o atribui a myAccount
+		
+		Account account1 = new Account("Jane Green", 50.00);
+
+		// Exibe saldo inicial de cada objeto
+		
+		System.out.printf("%s balance: $%.2f %n",
+				account1.getName(),account1.getBalance());
+
 		
 		// cria um objeto Scanner para receber entrada de dados do usuário
 		Scanner sc = new Scanner(System.in);
 		
-		// cria um objeto Account e o atribui a myAccount
+		System.out.print("Enten deposit amount for account1: "); // prompt
+		double depositAmount = sc.nextDouble(); // obtém a entrada do usuário
+		System.out.printf("%nadding %.2f to account1 balance%n%n",
+				depositAmount);
+		account1.deposit(depositAmount); // adicionada o saldo de account1
 		
-		Account myAccount = new Account();
+		//exibe os saldos
+		System.out.printf("%s balance: $%.2f %n",
+				account1.getName(),account1.getBalance());
 		
+		/*
 		// exibe o valor inicial do nome(null)
 		System.out.printf("Initial name is: %s%n%n", myAccount);
 		
@@ -28,6 +45,9 @@ public class AccountTest {
 		System.out.printf("Name in object myAccount is %s%n%n", myAccount.getName());
 		
 				
+		System.out.println("=========================================================");
+		
+		*/
 		
 		sc.close();
 
